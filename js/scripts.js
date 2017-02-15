eb7387e8ce78b0917f8657e825a7e60ca2cf08f1
Jónína Ósk Hansen <Jnna sk Hansen>
2017-02-15 09:25:36 +0000

    alls konar

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,45 @@ r.onreadystatechange = function () {
   if (r.readyState != 4 || r.status != 200) return;
 //  alert("Success: " + r.responseText);
 	const response = JSON.parse(r.responseText);
-	const container = document.createElement("div");
-	const image = document.createElement("img");
-	const title = document.createElement("h1");
-	//image.src = "http://image.tmdb.org/t/p/original" + response.backdrop_path;
-	title.innerHTML = response.original_title;
-	//container.append(image);
-	container.append(title);
-	document.getElementById("img").append(container);
+	// const container = document.createElement("div");
+	// const image = document.createElement("img");
+	// const title = document.createElement("h1");
+	// //image.src = "http://image.tmdb.org/t/p/original" + response.backdrop_path;
+	// title.innerHTML = response.original_title;
+	// //container.append(image);
+	// container.append(title);
+	document.getElementById("test").innerHTML=`
+	<div>${response.original_title}</div>
+	<div>Felicity Jones | Diego Luna | Forest Whitaker</div>
+	<div class="flex-bottom-align-apart">Action, Adventure, Sci-Fi <div class="pull-right number-circle">8.1</div></div>
+	`;
+	console.log(response);
 };
-r.send("banana=yellow");
+r.send();
 
+
+
+var r = new XMLHttpRequest(); //hluti af javascript
+r.open("GET", 'https://api.themoviedb.org/3/movie/550/credits?api_key=b121c7cc62271f6902d3cd115457a166', true);
+r.onreadystatechange = function () {
+  if (r.readyState != 4 || r.status != 200) return;
+//  alert("Success: " + r.responseText);
+	const response = JSON.parse(r.responseText);
+	// const container = document.createElement("div");
+	// const image = document.createElement("img");
+	// const title = document.createElement("h1");
+	// //image.src = "http://image.tmdb.org/t/p/original" + response.backdrop_path;
+	// title.innerHTML = response.original_title;
+	// //container.append(image);
+	// container.append(title);
+	document.getElementById("test").innerHTML=`
+	<div>${response.original_title}</div>
+	<div>Felicity Jones | Diego Luna | Forest Whitaker</div>
+	<div class="flex-bottom-align-apart">Action, Adventure, Sci-Fi <div class="pull-right number-circle">8.1</div></div>
+	`;
+	console.log(response);
+};
+r.send();
 
 
 
@@ -40,4 +68,5 @@ r.send("banana=yellow");
 }))
 
 ;
+
 
